ff781687e9de6d853457968f67994b4a049ebf06
aalexandr6 <aalexanderp6@gmail.com>
2023-03-13 00:27:06 -0500

    engineertest

---
+++ __tests__/engineerTest.js
@@ -0,0 +1,13 @@
+const Engineer = require('../lib/Engineer');
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Aaron', 1, 'aalexanderp6@gmail.com', 'aalexandr6');
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+test('gets engineer github value', () => {
+    const engineer = new Engineer('Aaron', 1, 'aalexanderp6@gmail.com', 'aalexandr6');
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+test('gets role of employee', () => {
+    const engineer = new Engineer('Aaron', 1, 'aalexanderp6@gmail.com', 'aalexandr6');
+    expect(engineer.getRole()).toEqual("Engineer");
+});
